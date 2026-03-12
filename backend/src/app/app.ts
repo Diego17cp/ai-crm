f29bb090cfde5bb6d7dc96d13abe4a7b2785c81a
Diego17 <diegoalbertocastropastor@gmail.com>
2026-03-11 19:36:30 -0500

    initialize backend with Express server and environment configuration

---
+++ backend/src/app/app.ts
@@ -0,0 +1,34 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+// import cookieParser from 'cookie-parser';
+import { env } from '@/config'
+// import { DatabaseConnection } from '@/config';
+// import router from './routes';
+// import { errorHandler, staticMiddleware } from './middlewares';
+
+
+export const app: Application = express();
+
+app.use(cors({
+    origin: env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', "X-Client-Platform"],
+    credentials: true,
+}))
+app.use(helmet());
+app.use(express.json());
+// app.use(cookieParser());
+
+app.get('/', (_, res) => {
+    res.json({
+        message: "API",
+        status: "running",
+        // database: DatabaseConnection.getInstance().getStatus(),
+        timestamp: new Date().toISOString()
+    });
+})
+
+// app.use("/api", router);
+// app.use(staticMiddleware);
+// app.use(errorHandler);
---
+++ backend/src/config/env.ts
@@ -0,0 +1,5 @@
+export const env = {
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 3000,
+    FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
+}
